36803c22f9c3767ac02b64d07761c68d8e1f05fc
SDC-M <182355397+SDC-M@users.noreply.github.com>
2025-03-11 11:16:01 +0100

    home page

--- frontend/js/home-page.js
+++ frontend/js/home-page.js
@@ -5,11 +5,16 @@ $("#dark-light-mode-button").on("click", function () {
         $("#dark-light-mode-button").removeClass(["fa-solid", "fa-sun"]);
         $("#dark-light-mode-button").addClass(["fa-regular", "fa-moon"]);
         $("body").css("background-color", "#f3f2f2");
+        $("#name").attr("src", "/frontend/img/name.png");
+        $("html").css("background-color", "#fefefe");
+        $("body").css("background-color", "#fefefe");
     } else {
         $("#container").removeClass("light-mode");
         $("#container").addClass("dark-mode");
         $("#ldark-light-mode-button").removeClass(["fa-regular", "fa-moon"]);
         $("#dark-light-mode-button").addClass(["fa-solid", "fa-sun"]);
         $("body").css("background-color", "rgb(128, 128, 128)");
+        $("html").css("background-color", "rgb(128, 128, 128)");
+        $("#name").attr("src", "/frontend/img/name-light.png");
     }
 });
