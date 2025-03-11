18ce013a3a9286d49b927111ea484d5fa796044b
SDC-M <182355397+SDC-M@users.noreply.github.com>
2025-03-11 17:51:15 +0100

    theme transmission

--- frontend/js/home-page.js
+++ frontend/js/home-page.js
@@ -1,20 +1,34 @@
-$("#dark-light-mode-button").on("click", function () {
-    if ($("#container").hasClass("dark-mode")) {
-        $("#container").removeClass("dark-mode");
-        $("#container").addClass("light-mode");
-        $("#dark-light-mode-button").removeClass(["fa-solid", "fa-sun"]);
-        $("#dark-light-mode-button").addClass(["fa-regular", "fa-moon"]);
-        $("body").css("background-color", "#f3f2f2");
-        $("#name").attr("src", "/frontend/img/name.png");
-        $("html").css("background-color", "#fefefe");
-        $("body").css("background-color", "#fefefe");
+$(document).ready(function () {
+    if (localStorage.getItem('theme') === 'dark') {
+        dark_theme();
     } else {
-        $("#container").removeClass("light-mode");
-        $("#container").addClass("dark-mode");
-        $("#ldark-light-mode-button").removeClass(["fa-regular", "fa-moon"]);
-        $("#dark-light-mode-button").addClass(["fa-solid", "fa-sun"]);
-        $("body").css("background-color", "rgb(128, 128, 128)");
-        $("html").css("background-color", "rgb(128, 128, 128)");
-        $("#name").attr("src", "/frontend/img/name-light.png");
+        light_theme();
     }
+
+    $("#dark-light-mode-button").on("click", function () {
+        if ($("#container").hasClass("dark-mode")) {
+            light_theme();
+            localStorage.setItem('theme', 'light');
+        } else {
+            dark_theme();
+            localStorage.setItem('theme', 'dark');
+        }
+    });
 });
+
+function dark_theme() {
+    $("#container").removeClass("light-mode").addClass("dark-mode");
+    $("#dark-light-mode-button").removeClass(["fa-regular", "fa-moon"]).addClass(["fa-solid", "fa-sun"]);
+    $("body").css("background-color", "rgb(128, 128, 128)");
+    $("html").css("background-color", "rgb(128, 128, 128)");
+    $("#name").attr("src", "/frontend/img/name-light.png");
+}
+
+function light_theme() {
+    $("#container").removeClass("dark-mode").addClass("light-mode");
+    $("#dark-light-mode-button").removeClass(["fa-solid", "fa-sun"]).addClass(["fa-regular", "fa-moon"]);
+    $("body").css("background-color", "#f3f2f2");
+    $("html").css("background-color", "#fefefe");
+    $("body").css("background-color", "#fefefe");
+    $("#name").attr("src", "/frontend/img/name.png");
+}
