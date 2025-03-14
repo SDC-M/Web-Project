02a2f7c00244172813cdb9d0920e332495f90304
Rheydskey <matgag@tutanota.com>
2025-03-14 18:06:24 +0100

    add FormValidator

---
+++ src/Handler/Utils/FormHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kuva\Handler\Utils;
+
+// use Kuva\Utils\Router\Handler;
+// use Kuva\Utils\Router\Request;
+// use Kuva\Utils\Router\Response;
+
+// abstract class FormHandler extends Handler {
+//     public function handle(Request $req): void
+//     {
+
+//     }
+
+//     abstract function validatedHandle(Request $req, FormData $form): Response;
+//     abstract function badFormHandle(Request $req): Response;
+// }
---
+++ src/Utils/FormValidator.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Kuva\Utils;
+
+class FormValidator
+{
+    private array $fields = [];
+
+    public static function fileValidator(string $name): ?array
+    {
+        if (!isset($_FILES[$name])) {
+            return null;
+        }
+
+        return $_FILES[$name];
+    }
+
+
+    public static function textValidator(string $name): ?string
+    {
+        if (!isset($_POST[$name])) {
+            return null;
+        }
+
+        return $_POST[$name];
+    }    
+
+    /**
+     * @param callable(): mixed $validator
+     */
+    public function addCustomValidatorField(string $name, callable $validator): void
+    {
+        $this->fields[$name] = $validator;
+    }
+
+    public function addTextField(string $name): FormValidator
+    {
+        $this->addCustomValidatorField(
+            $name,
+            function (string $name) {
+                return FormValidator::textValidator($name);
+            }
+        );
+
+        return $this;
+    }
+
+    public function addFileField(string $name): FormValidator
+    {
+        $this->addCustomValidatorField(
+            $name,
+            function (string $name) {
+                return FormValidator::fileValidator($name);
+            }
+        );
+
+        return $this;
+    }
+
+    public function validate(): bool|array {
+        $array = [];
+        foreach ($this->fields as $key => $validator) {
+            $value = $validator($key);
+            if ($value == null) {
+                return false;
+            }
+            $array[$key] = $value;
+        }
+
+        return $array;
+    }
+}
