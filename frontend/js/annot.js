e2a69bd6e428eb0110c7f95c3bd9d97d5342edd5
SDC-M <182355397+SDC-M@users.noreply.github.com>
2025-03-18 21:53:36 +0100

    add annotation system

--- frontend/js/annot.js
+++ frontend/js/annot.js
@@ -35,6 +35,19 @@ function convertTabPoints(tab, realWidth, printWidth, realHeight, printHeight) {
     return tab2;
 }
 
+function updateCoords(x1, y1, x2, y2) {
+    $('#x1Coord').val(x1);
+    $('#y1Coord').val(y1);
+    $('#x2Coord').val(x2);
+    $('#y2Coord').val(y2);
+}
+
+function dark_theme() {
+    $("#page-container").removeClass("light-mode").addClass("dark-mode");
+    $("body, html").css("background-color", "rgb(128, 128, 128)");
+    localStorage.setItem('theme', 'dark');
+}
+
 $(document).ready(function () {
     $(window).resize(function () {
         resizeCanvas();
@@ -61,16 +74,23 @@ $(document).ready(function () {
                 const x2 = points[1].x;
                 const y2 = points[1].y;
 
-                ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+                ctx.fillStyle = 'rgba(158, 68, 80, 0.5)';
                 ctx.fillRect(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x2 - x1), Math.abs(y2 - y1));
-                console.log(convertTabPoints(points, $('#image')[0].naturalWidth, $('#image')[0].clientWidth, $('#image')[0].naturalHeight, $('#image')[0].clientHeight));
+                let co = convertTabPoints(points, $('#image')[0].naturalWidth, $('#image')[0].clientWidth, $('#image')[0].naturalHeight, $('#image')[0].clientHeight);
+                updateCoords(co[0].xCalc, co[0].yCalc, co[1].xCalc, co[1].yCalc);
             }
         }
     });
     $('#reset').on('click', function () {
         points = [];
+        $('#x1Coord').val('');
+        $('#y1Coord').val('');
+        $('#x2Coord').val('');
+        $('#y2Coord').val('');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     });
 });
 
-
+if (localStorage.getItem('theme') === 'dark') {
+    dark_theme();
+}
