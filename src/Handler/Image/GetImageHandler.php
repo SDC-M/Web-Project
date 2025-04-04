1e9d6fdd2d73e15705d2063bf118a2b3ec75129e
Rheydskey <matgag@tutanota.com>
2025-04-04 17:05:34 +0200

    Fix image endpoint

--- src/Handler/Image/GetImageHandler.php
+++ src/Handler/Image/GetImageHandler.php
@@ -3,7 +3,7 @@
 namespace Kuva\Handler\Image;
 
 use Kuva\Backend\Middleware\ImageMiddleware;
-use Kuva\Backend\Middleware\UserMiddleware;
+use Kuva\Backend\User;
 use Kuva\Utils\Router\Handler;
 use Kuva\Utils\Router\Request;
 use Kuva\Utils\Router\Response;
@@ -13,10 +13,14 @@ class GetImageHandler extends Handler
     public function handle(Request $req): void
     {
         $image = ImageMiddleware::getFromUrl($req);
-        $user = UserMiddleware::getFromSession();
 
-        if (!$image->is_public && $user != $image->owner->id) {
-            return;
+
+        if (!$image->is_public) {
+            $user = User::getFromSession();
+            if ($user == null || $user->id != $image->owner->id) {
+                $req = new Response(404);
+                return;               
+            }
         }
 
         $this->response = new Response(200, $image->getBytes(), ["Content-Type" => "image/png"]);
