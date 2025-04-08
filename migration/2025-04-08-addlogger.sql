71d1f7597e4f742cb4693f7826de20147e0ac062
Rheydskey <matgag@tutanota.com>
2025-04-08 11:19:22 +0200

    Add set null on delete cascade

--- migration/2025-04-08-addlogger.sql
+++ migration/2025-04-08-addlogger.sql
@@ -2,8 +2,8 @@ CREATE TABLE `logs` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `description` text NOT NULL,
   `creation_date` datetime NOT NULL,
-  `executed_by` int(10) unsigned NOT NULL,
+  `executed_by` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `logs_users_FK` (`executed_by`),
-  CONSTRAINT `logs_users_FK` FOREIGN KEY (`executed_by`) REFERENCES `users` (`id`)
+  CONSTRAINT `logs_users_FK` FOREIGN KEY (`executed_by`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
