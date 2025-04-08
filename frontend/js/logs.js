2d65135644483483faf0736b4af3012a05ac405c
SDC-M <182355397+SDC-M@users.noreply.github.com>
2025-04-08 16:48:28 +0200

    specification

--- frontend/js/logs.js
+++ frontend/js/logs.js
@@ -1,5 +1,14 @@
 const { transformDate } = await import("./data-treatment.mjs");
 
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+/**
+ * @param afterId 
+ *  Tente d'afficher les logs à partir de l'indice passé en paramètre dans 
+ *   l'ensemble de tous les logs. En cas d'échec renvoie l'erreur 
+ *   correspondante.
+ */
 async function getLogs(afterId) {
     const url = `/api/logs?after=${afterId}`;
     try {
@@ -28,6 +37,12 @@ async function getLogs(afterId) {
     }
 }
 
+/**
+ * @param afterId
+ *  Tente de mettre a jour la pagination avec l'id passé en paramètre, en cas
+ *   de succès efface l'ancien tableau, le met à jour puis met à jour l'index
+ *   en cas d'échec renvoie l'erreur correspondante.
+ */
 function updatePagination(afterId) {
     const $pagesContainer = $("#pages");
     $pagesContainer.empty();
@@ -37,6 +52,9 @@ function updatePagination(afterId) {
     });
     $pagesContainer.append($nextPage);
 }
+
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
 
 $(document).ready(async function () {
     $("#global-loader").show();
