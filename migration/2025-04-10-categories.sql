5cb4e92a26340f30564e4ee27e5de7f2e45ef9e0
Rheydskey <matgag@tutanota.com>
2025-04-11 16:24:35 +0200

    On delete cascade to migration file

--- migration/2025-04-10-categories.sql
+++ migration/2025-04-10-categories.sql
@@ -5,11 +5,11 @@ CREATE TABLE `categories` (
    CONSTRAINT UC_Name UNIQUE (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE image_categorie (
-	image_id INT UNSIGNED NOT NULL,
-	categorie_id INT UNSIGNED NOT NULL,
-	CONSTRAINT image_categories_images_FK FOREIGN KEY (image_id) REFERENCES kuva.images(id),
-	CONSTRAINT image_categories_categories_FK FOREIGN KEY (categorie_id) REFERENCES kuva.categories(id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `image_categorie` (
+  `image_id` int(10) unsigned NOT NULL,
+  `categorie_id` int(10) unsigned NOT NULL,
+  KEY `image_categories_categories_FK` (`categorie_id`),
+  KEY `image_categories_images_FK` (`image_id`),
+  CONSTRAINT `image_categories_categories_FK` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `image_categories_images_FK` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
