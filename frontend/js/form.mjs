bc2f3d6a186a8b0e8956cd19f9e6c2e31c373adc
SDC-M <182355397+SDC-M@users.noreply.github.com>
2025-04-15 18:54:22 +0200

    WIP: formHAndler js

--- frontend/js/form.mjs
+++ frontend/js/form.mjs
@@ -1,17 +1,7 @@
 export function onSubmit(form, handler) {
-  form.addEventListener("submit", function (e) {
+  form.on("submit", function (e) {
     e.preventDefault();
-    const data = new FormData(form);
+    const data = new FormData(form[0]);
     handler(data);
   });
-}
-
-export async function sendPost(url, body, onSuccess, onFailure) {
-  let req = await fetch(url, { method: "POST", body: body });
-  if (req.ok) {
-    form.getElementsByClassName("error")[0].innerHTML = "ok";
-    window.location.replace("/");
-  } else {
-    form.getElementsByClassName("error")[0].innerHTML = "Erreur";
-  }
 }
