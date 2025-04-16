0d0fcbdbebad94ccff3d0aca9ed0be0e708b549b
SDC-M <182355397+SDC-M@users.noreply.github.com>
2025-04-16 10:51:22 +0200

    remove useless attribute in administrator table

--- migration/2025-04-16-administrators.sql
+++ migration/2025-04-16-administrators.sql
@@ -1,5 +1,4 @@
 CREATE TABLE `administrators` (
-  `id` int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `user_id` int (10) unsigned NOT NULL,
   FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
