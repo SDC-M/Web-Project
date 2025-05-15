3cba23e6b1b3d98b889e3c485cf8761cb8712803
SDC-M <debacksebastien@gmail.com>
2025-05-15 15:12:22 +0200

    home page clean up (remove slide-image)

--- frontend/js/home-page.js
+++ frontend/js/home-page.js
@@ -3,25 +3,7 @@ import { setLocalStorageTheme, setButtonSwitchTheme } from "./theme.mjs";
 /* --------------------------------------------------------------------- */
 /* --------------------------------------------------------------------- */
 
-/**
- * Anime le caroussel.
- */
-function rotateSlides() {
-  var $firstSlide = $("#carousel").find("div:first");
-  var width = $firstSlide.width();
-
-  $firstSlide.animate({ marginLeft: -width }, 1000, function () {
-    var $lastSlide = $("#carousel").find("div:last");
-    $lastSlide.after($firstSlide);
-    $firstSlide.css({ marginLeft: 0 });
-  });
-}
-
-/* --------------------------------------------------------------------- */
-/* --------------------------------------------------------------------- */
-
 $(document).ready(function () {
-  window.setInterval(rotateSlides, 3000);
   setLocalStorageTheme();
   setButtonSwitchTheme();
 });
